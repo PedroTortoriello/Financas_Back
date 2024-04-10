46c0bf627bf58c96822263426f78c5bf69693b4e
Pedro TortoriellO <pedrooofreitas@gmail.com>
2024-04-10 14:19:25 -0400

    10/04

--- routes/autenticacao.js
+++ routes/autenticacao.js
@@ -21,7 +21,7 @@ const corsOptions = {
 };
 
 // Movendo a configuração de CORS antes das definições de rota
-app.use(cors(corsOptions));
+app.use(cors());
 
 router.post("/autenticacao", async function (req, res) {
   try{
